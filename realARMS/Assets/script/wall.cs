3d748edc79b082afbb43fd7bc1fe30d191aca4b6
nishida1241 <i141323@gm.ishikawa-nct.ac.jp>
2017-08-01 14:42:08 +0900

    kabi_Game
    カービィのゲームを練習で作りました

---
+++ realARMS/Assets/script/wall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wall : MonoBehaviour {
+	public float speed = 1.0f;
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, 10f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (speed * Time.deltaTime, 0, 0);
+	}
+}
