f27bab95b3efa7acbe9434acd216d6dfaaffd293
mizutamaboy <i141326@gm.ishikawa-nct.ac.jp>
2017-08-02 16:55:10 +0900

    unitychan XX
    unityちゃんが2人(動きは連動している)

---
+++ Unity Project 2/Assets/Kinect Avatar.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public GameObject Ref;
+	public GameObject Hips;
+	public GameObject LeftUpLeg;
+	public GameObject LeftLeg;
+	public GameObject RightUpLeg;
+	public GameObject RightLeg;
+	public GameObject Spine1;
+	public GameObject Spine2;
+	public GameObject LeftShoulder;
+	public GameObject LeftArm;
+	public GameObject LeftForeArm;
+	public GameObject LeftHand;
+	public GameObject RightShoulder;
+	public GameObject RightArm;
+	public GameObject RightForeArm;
+	public GameObject RightHand;
+	public GameObject Neck;
+	public GameObject Head;
+	// Use this for initialization
+	void Start () {
+		
+		Ref = 
+			_UnityChan.transform.FindChild( "Character1_Reference" ).gameObject;
+		Hips = 
+			Ref.gameObject.transform.FindChild( "Character1_Hips" ).gameObject;
+		LeftUpLeg = 
+			Hips.transform.FindChild( "Character1_LeftUpLeg" ).gameObject;
+		LeftLeg = 
+			LeftUpLeg.transform.FindChild( "Character1_LeftLeg" ).gameObject;
+		RightUpLeg = 
+			Hips.transform.FindChild( "Character1_RightUpLeg" ).gameObject;
+		RightLeg = 
+			RightUpLeg.transform.FindChild( "Character1_RightLeg" ).gameObject;
+		Spine1 = 
+			Hips.transform.FindChild( "Character1_Spine" ).gameObject.
+			transform.FindChild( "Character1_Spine1" ).gameObject;
+		Spine2 = 
+			Spine1.transform.FindChild( "Character1_Spine2" ).gameObject;
+		LeftShoulder = 
+			Spine2.transform.FindChild( "Character1_LeftShoulder" ).gameObject;
+		LeftArm = 
+			LeftShoulder.transform.FindChild( "Character1_LeftArm" ).gameObject;
+		LeftForeArm = 
+			LeftArm.transform.FindChild( "Character1_LeftForeArm" ).gameObject;
+		LeftHand = 
+			LeftForeArm.transform.FindChild( "Character1_LeftHand" ).gameObject;
+		RightShoulder = 
+			Spine2.transform.FindChild( "Character1_RightShoulder" ).gameObject;
+		RightArm = 
+			RightShoulder.transform.FindChild( "Character1_RightArm" ).gameObject;
+		RightForeArm = 
+			RightArm.transform.FindChild( "Character1_RightForeArm" ).gameObject;
+		RightHand = 
+			RightForeArm.transform.FindChild( "Character1_RightHand" ).gameObject;
+		Neck = 
+			Spine2.transform.FindChild( "Character1_Neck" ).gameObject;
+		Head = 
+			Neck.transform.FindChild( "Character1_Head" ).gameObject;
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ UnityChan-meet-Kinect-master/README.md
@@ -0,0 +1,32 @@
+ユニティちゃんをKinect for Windows v1で動かす
+============
+
+ユニティちゃんをKinectで動かします。Kinect for Windows v1で動きます。
+
+[![ScreenShot](https://raw.githubusercontent.com/kaorun55/UnityChan-meet-Kinect/master/Screenshot.png)](http://youtu.be/7CrhJpf-Jec)
+
+ * unitychan\Assets\unitychan.unity を起動してください
+
+## 環境
+
+ * Unity Pro 4.5
+ * Visual Studio 2013 Ultimate(UnityVs)
+ * [ユニティちゃん](http://unity-chan.com/)
+ * [Kinect for Windows SDK v1.8](http://www.microsoft.com/en-us/kinectforwindowsdev/default.aspx)
+ * [Kinect Wrapper Package for Unity3D](http://wiki.etc.cmu.edu/unity3d/index.php/Microsoft_Kinect_-_Microsoft_SDK) (このパッケージではKinect for Windows SDK v1.7に対応)
+
+##ライセンス
+
+ * ユニティちゃんは「[ユニティちゃんライセンス](http://unity-chan.com/)」にて使用しています
+
+![ScreenShot](https://raw.githubusercontent.com/kaorun55/UnityChan-meet-Kinect/master/Light_Silhouette.png)
+
+本コンテンツは、『[ユニティちゃんライセンス条項](http://unity-chan.com/download/license.html)』に基づいて公開・配布されるものです。本コンテンツをご利用される場合は、ユニティ・テクノロジーズ・ジャパン合同会社が定める『[キャラクター利用のガイドライン](http://unity-chan.com/download/guideline.html)』をお守りいただく必要があります。
+
+
+## フィードバックくださいｗ
+
+ * フォルダ構成など、こうしたほうがいいよというのがたくさんあるかと思います。
+ * ライティング
+ * ボーンの設定(左右が逆とか、位置が違うとか)
+ * Unity Freeで動くかどうか
