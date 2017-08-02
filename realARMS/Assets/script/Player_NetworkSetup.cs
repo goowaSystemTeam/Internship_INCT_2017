9dfca7b8f9821bbdaa5e94671759c67cf46ebf6a
nishida1241 <i141323@gm.ishikawa-nct.ac.jp>
2017-08-02 14:20:18 +0900

    UNET_TEST
    通信のテストです

---
+++ realARMS/Assets/script/Player_NetworkSetup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player_NetworkSetup : NetworkBehaviour {
+	[SerializeField]Camera FPSCharactorCam;
+	[SerializeField]AudioListener audioListener;
+
+	// Use this for initialization
+	void Start () {
+		if (isLocalPlayer) {
+			GameObject.Find ("Scene Camera").SetActive (false);
+			GetComponent<CharacterController> ().enabled = true;
+			GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController> ().enabled = true;
+			FPSCharactorCam.enabled = true;
+			audioListener.enabled = true;
+		}
+	}
+}
---
+++ realARMS/Assets/script/Player_SyncRotation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player_SyncRotation : NetworkBehaviour {
+
+	[SyncVar]private Quaternion syncPlayerRotation;
+	[SyncVar]private Quaternion syncCamRotation;
+
+	[SerializeField]private Transform playerTransform;
+	[SerializeField]private Transform camTransform;
+	[SerializeField]private float lerpRate = 15;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void FixedUpdate(){
+		TransmitRotations ();
+		LerpRotations ();
+	}
+
+	void LerpRotations(){
+		if(!isLocalPlayer){
+			playerTransform.rotation = Quaternion.Lerp (playerTransform.rotation,
+				syncPlayerRotation, Time.deltaTime * lerpRate);
+			camTransform.rotation = Quaternion.Lerp (camTransform.rotation,
+				syncCamRotation, Time.deltaTime * lerpRate);
+		}
+	}
+
+	[Command]
+	void CmdProvideRotationsToServer(Quaternion playerRot,Quaternion camRot){
+		syncPlayerRotation = playerRot;
+		syncCamRotation = camRot;
+	}
+
+	[Client]
+	void TransmitRotations(){
+		if(isLocalPlayer){
+			CmdProvideRotationsToServer (playerTransform.rotation, camTransform.rotation);
+		}
+	}
+}
