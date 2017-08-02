4b551f583dd1cd3de669cc9c03bd6a811aaf4c70
nishida1241 <i141323@gm.ishikawa-nct.ac.jp>
2017-08-02 16:41:15 +0900

    sepalate_window
    画面分けました

---
+++ realARMS/Assets/script/Player_Camera_Set.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Camera_Set : MonoBehaviour {
+	[Range(0.1f,10.0f)]public float parallax = 1.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnValidate(){
+		var position = new Vector3 (parallax, 0, 0);
+		GameObject.Find ("FPCamL").transform.localPosition = -position;
+		GameObject.Find ("FPCamR").transform.localPosition = position;
+	}
+}
--- realARMS/Assets/script/Player_NetworkSetup.cs
+++ realARMS/Assets/script/Player_NetworkSetup.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class Player_NetworkSetup : NetworkBehaviour {
-	[SerializeField]Camera FPSCharactorCam;
 	[SerializeField]AudioListener audioListener;
+	[SerializeField]Camera FPCam;
 
 	// Use this for initialization
 	void Start () {
 		if (isLocalPlayer) {
 			GameObject.Find ("Scene Camera").SetActive (false);
+			GameObject.Find ("FirstPersonCharacter/FPCamL").GetComponent<Camera> ().enabled = true;
+			GameObject.Find ("FirstPersonCharacter/FPCamR").GetComponent<Camera> ().enabled = true;
 			GetComponent<CharacterController> ().enabled = true;
 			GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController> ().enabled = true;
-			FPSCharactorCam.enabled = true;
 			audioListener.enabled = true;
+			FPCam.enabled = true;
 		}
 	}
 }
