8ba2d85f359a49f72fe4db64c4d51056f67b35ed
KUPPA <30610526+799178752@users.noreply.github.com>
2017-08-02 10:50:36 +0900

    ｄ
    s

---
+++ ARMS/Assets/UnityChan/Scripts/Sphere.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0, 0, speed * Time.deltaTime);
+	}
+}
---
+++ ARMS/Assets/UnityChan/Scripts/spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	public Vector3 Hose;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Jump")) {
+			Instantiate (bulletPrefab, transform.position + Hose, transform.rotation);
+
+		}
+	}
+}
